b6c44690706674affd33eb6fb6be21cc75c4eac7
Dan <dchen12812@gmail.com>
2015-04-26 17:24:24 -0700

    Skew tests

---
+++ Random Bioinfo/apprPatMat.py
@@ -0,0 +1,8 @@
+'''
+Approximate Pattern Matching Problem: Find all approximate occurrences of a pattern in a string.
+     Input: Strings Pattern and Text along with an integer d.
+     Output: All starting positions where Pattern appears as a substring of Text with at most d mismatches.
+'''
+
+def apprPatMat():
+	pass
--- Random Bioinfo/skew.py
+++ Random Bioinfo/skew.py
@@ -2,4 +2,22 @@
 # G += 1, C-= 1
 
 def skew(string):
-	pass
+	num = 0
+	output = "0 "
+
+	for i in string:
+		
+		if i == 'C':
+			num -= 1
+		elif i == 'G':
+			num += 1
+		output += str(num) + " "
+
+	return output
+
+text = "CATGGGCATCGGCCATACGCC"
+#0 -1 -1 -1 0 1 2 1 1 1 0 1 2 1 0 0 0 0 -1 0 -1 -2
+print(skew(text))
+
+text = " TAAAGACTGCCGAGAGGCCAACACGAGTGCTAGAACGAGGGGCGTAAACGCGGGTCCGAT"
+print(skew(text))
