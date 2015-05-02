59ff3f83aa25b8c41febad9eb39dda22205a42df
Dan <dchen12812@gmail.com>
2015-05-02 15:17:04 -0700

    completed function
    Seems to do what is intended for 1-3 k

--- Test/seqeunces.py
+++ Test/seqeunces.py
@@ -2,6 +2,35 @@
 
 from itertools import combinations_with_replacement
 
+
 def sequences(k):
 	seqList = {}
+	nuc = 'ATGC'
+	seqs = combinations_with_replacement(nuc,k)
+
+	for i in seqs:
+		kmer = ""
+		for begin in range(k):
+			kmer += i[begin]
+		seqList[kmer] = 0
+
+	return seqList
+
 #test out the usage of combinations with replacement
+
+
+test = sequences(2)
+
+for key, value in test.items():
+	print(key)
+
+'''
+test  = combinations_with_replacement('ATGC',2)
+fix = []
+for i in test:
+	print(i[0:len(i)])
+	fix.append(i[0] + i[1])
+
+for i in fix:
+	print(i)
+'''
