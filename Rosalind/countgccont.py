9e03223afd46a58d89bfb1bc4571a2550c0ab3ab
Dan <dchen12812@gmail.com>
2015-05-07 14:24:31 -0700

    updates

--- Rosalind/countgccont.py
+++ Rosalind/countgccont.py
@@ -3,18 +3,37 @@
 def countgccont():
 	f = open('rosalind_gc.txt', 'r')
 	lastkey = ""
+	total = 0
+	gc = 0
 	fastaseq = {}
 	print(f.readlines()[0:20])
 	#Want to seaprate out files and store rosalind : sequence
 	#Need to parse out the \n
+	#alt is to scan gc and add to last one but would need a total length counter
 	for line in f:
 		#Check 1st 8 char for rosalind, != then store it as part of dict
-		if line[0:7] == "Rosalind":
-			#Make a blank key for dict
-			#lastkey = whatever this line is
+		if line[1:8] == "Rosalind":
+			if total == 0:
+				fastaseq[line] = 0
+				lastkey = line
+			else:
+				fastaseq[lastkey] = gc/total
+				gc = 0
+				total = 0
 		else:
+			total += len(line)
+			for nuc in line:
 			#Need a way to know it is part of that key then input it
 			#fastaseq[lastkey] = line
+				if nuc == 'G' or nuc == 'C':
+					gc += 1
+	
+	answer = ""
+	for key, value in fastaseq:
+		if max(fastaseq) == value:
+			answer = key + '\n' + value
+
+	return answer
 
 
 
