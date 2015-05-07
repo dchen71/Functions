f3ebfafa9ed5fa1524a9c4afd2a2745652ffe182
Dan <dchen12812@gmail.com>
2015-05-07 14:59:53 -0700

    updates

--- Rosalind/countgccont.py
+++ Rosalind/countgccont.py
@@ -1,4 +1,6 @@
 #Counts the percent of GC content in a series of Fasta and return the fasta id with highest content and percentage
+#Need to figure out why the incorrect value is returned for gc content
+import operator
 
 def countgccont():
 	f = open('rosalind_gc.txt', 'r')
@@ -6,35 +8,44 @@ def countgccont():
 	total = 0
 	gc = 0
 	fastaseq = {}
-	print(f.readlines()[0:20])
-	#Want to seaprate out files and store rosalind : sequence
-	#Need to parse out the \n
-	#alt is to scan gc and add to last one but would need a total length counter
+	#Checks each line
 	for line in f:
-		#Check 1st 8 char for rosalind, != then store it as part of dict
-		if line[1:8] == "Rosalind":
+		#Checks if the line starts as Rosalind
+		if line[1:9] == "Rosalind":
+			#First time running function
 			if total == 0:
 				fastaseq[line] = 0
 				lastkey = line
+			#If if ran before, computes the gc/total
 			else:
 				fastaseq[lastkey] = gc/total
 				gc = 0
 				total = 0
+				fastaseq[line] = 0
+				lastkey = line
 		else:
 			total += len(line)
 			for nuc in line:
-			#Need a way to know it is part of that key then input it
-			#fastaseq[lastkey] = line
 				if nuc == 'G' or nuc == 'C':
 					gc += 1
-	
-	answer = ""
-	for key, value in fastaseq:
-		if max(fastaseq) == value:
-			answer = key + '\n' + value
+	fastaseq[lastkey] = gc/total
 
-	return answer
+	return (max(fastaseq.items(),key=operator.itemgetter(1)))
 
+'''
+>Rosalind_6404
+CCTGCGGAAGATCGGCACTAGAATAGCCAGAACCGTTTCTCTGAGGCTTCCGGCCTTCCC
+TCCCACTAATAATTCTGAGG
+>Rosalind_5959
+CCATCGGTAGCGCATCCTTAGTCCAATTAAGTCCCTATCCAGGCGCTCCGCCGAAGGTCT
+ATATCCATTTGTCAGCAGACACGC
+>Rosalind_0808
+CCACCCTCGTGGTATGGCTAGGCATTCAGGAACCGGAGAACGCTTCAGACCAGCCCGGAC
+TGGGAACCTGCGGGCAGTAGGTGGAAT
+
+#Rosalind_0808
+60.919540
+'''
 
 
 print(countgccont())
