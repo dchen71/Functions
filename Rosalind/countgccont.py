6c965010a113cfc8c303363ae11ae4e41521ccd0
Dan <dchen12812@gmail.com>
2015-05-08 21:44:57 -0700

    updated and finshed functions

--- Rosalind/countgccont.py
+++ Rosalind/countgccont.py
@@ -8,31 +8,31 @@ def countgccont():
 	total = 0
 	gc = 0
 	fastaseq = {}
+	
 	#Checks each line
 	for line in f:
+		print(line)
 		#Checks if the line starts as Rosalind
 		if line[1:9] == "Rosalind":
 			#First time running function
 			if total == 0:
-				fastaseq[line] = 0
-				lastkey = line
+				fastaseq[line[1:len(line) - 1]] = 0
+				lastkey = line[1:len(line) - 1]
 			#If if ran before, computes the gc/total
 			else:
-				fastaseq[lastkey] = gc/(total - 1)
-				print(total)
+				fastaseq[lastkey] = gc/(total) * 100
 				gc = 0
 				total = 0
 				fastaseq[line] = 0
-				lastkey = line
+				lastkey = line[1:len(line) - 1]
 		else:
-			total += len(line)
+			total += len(line) - 1 #each /n cuonts as a space and at end of each line
 			for nuc in line:
 				if nuc == 'G' or nuc == 'C':
 					gc += 1
-	print(total)
-	fastaseq[lastkey] = gc/(total - 1)
+	fastaseq[lastkey] = gc/(total + 1) * 100 #total+1 cause last line doesnt have /n and this here cause dont compute at end of loop
 
-	return (max(fastaseq.items(),key=operator.itemgetter(1)))
+	return (max(fastaseq.items(),key=operator.itemgetter(1))) #not formated but highest value
 
 '''
 >Rosalind_6404
@@ -48,6 +48,5 @@ TGGGAACCTGCGGGCAGTAGGTGGAAT
 #Rosalind_0808
 60.919540
 '''
-
 
 print(countgccont())
