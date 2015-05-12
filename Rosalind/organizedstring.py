7d5fd6ef9c0c89eabce4cee7c708f54da3c7c8ec
Dan <dchen12812@gmail.com>
2015-05-12 14:26:28 -0700

    added solution to function

--- Rosalind/organizedstring.py
+++ Rosalind/organizedstring.py
@@ -5,19 +5,37 @@ import itertools
 
 def organizedstring(string, length):
 	#break down string and remove the spaces between char
-	#permutations directly should work
 	nucleotides = string.replace(" ", "")
-	answer  = []
-	
-	answer = itertools.permutations(nucleotides, 2)
-	#none of the itertools can accurately pull it off, need to make a algorithm to do it
-	#look up the documentation on how itertools were made
+	answer = []
 
-	for i in answer:
+	#pool = tuple(nucleotides) #tuples are immutable list, required if going yield route
+	n = len(nucleotides)
+	r = length
+	for indices in itertools.product(range(n), repeat = r): #product(size of list, length of returned product)
+		kmer = ""
+		for i in indices: #uses the indicecs to build the kmer to add to answer
+			kmer += nucleotides[i]
+		answer.append(kmer)
+		'''
+		if len((indices)) == r: #makes sure the size of the unique value matches r, use set with indices to exclude repeats of same char
+			yield tuple(pool[i] for i in indices) #returns the tuple list of kmers
+		'''
+	for i in answer: #prints each answer out
 		print(i)
 
-	return answer
+	return answer #returns the array in case
 
+'''
 sample = "T A G C"
 k = 2
-print(organizedstring(sample,k))
+#required if going the yield route
+#test = organizedstring(sample,k)
+#for i in test:
+#	print(i)
+organizedstring(sample,k)
+'''
+
+print("")
+sample = "E N O F C V D L"
+k = 3
+organizedstring(sample,k)
