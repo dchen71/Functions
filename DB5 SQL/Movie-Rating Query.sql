eea4c4fab1c5fb7bf2bbbc6626f89862fe14b363
Dan <dchen12812@gmail.com>
2015-05-13 22:06:05 -0700

    added solutions

--- DB5 SQL/Movie-Rating Query.sql
+++ DB5 SQL/Movie-Rating Query.sql
@@ -2,135 +2,122 @@
 SQLite Syntax
 
 Schema:
-Movie ( mID, title, year, director )
-English: There is a movie with ID number mID, a title, a release year, and a director.
+Highschooler ( ID, name, grade )
+English: There is a high school student with unique ID and a given first name in a certain grade.
 
-Reviewer ( rID, name )
-English: The reviewer with ID number rID has a certain name.
+Friend ( ID1, ID2 )
+English: The student with ID1 is friends with the student with ID2. Friendship is mutual, so if (123, 456) is in the Friend table, so is (456, 123).
 
-Rating ( rID, mID, stars, ratingDate )
-English: The reviewer rID gave the movie mID a number of stars rating (1-5) on a certain ratingDate. 
+Likes ( ID1, ID2 )
+English: The student with ID1 likes the student with ID2. Liking someone is not necessarily mutual, so if (123, 456) is in the Likes table, there is no guarantee that (456, 123) is also present. 
  */
 
 /*
 Q1:
-Find the titles of all movies directed by Steven Spielberg. 
- */
-
-SELECT title
-FROM Movie
-WHERE director = "Steven Spielberg"
-
-/*
-Q2:
-Find all years that have a movie that received a rating of 4 or 5, and sort them in increasing order. 
- */
-
-SELECT distinct year 
-FROM Movie join Rating 
-on Rating.mID = Movie.mID 
-WHERE Rating.stars >= 4
-ORDER BY year
-
-/*
-Q3:
-Find the titles of all movies that have no ratings. 
- */
-
-SELECT title
-FROM Movie
-WHERE mID not in (SELECT mID
-                  FROM Rating)
-
-/*
-Q4:
-Some reviewers didn't provide a date with their rating. Find the names of all reviewers who have ratings with a NULL 
-value for the date. 
+Find the names of all students who are friends with someone named Gabriel. 
  */
 
 SELECT name
-FROM Reviewer join Rating
-on Rating.rID = Reviewer.rID
-WHERE ratingDate is NULL
+FROM Highschooler H 
+  Join Friend on
+  Friend.id1 = H.id
+WHERE id2 in (SELECT id
+              FROM Highschooler
+              WHERE name = 'Gabriel')
+
+/*
+Q2:
+For every student who likes someone 2 or more grades younger than themselves, 
+return that student's name and grade, and the name and grade of the student they like. 
+ */
+
+SELECT H.name, H.grade, H2.name, H2.grade
+FROM Highschooler H 
+  JOIN Likes ON
+  Likes.id1 = H.id 
+  JOIN Highschooler H2 ON
+  H2.id = Likes.id2
+WHERE H2.grade <= H.grade - 2
+
+/*
+Q3:
+For every pair of students who both like each other, return the name and grade of both students. 
+Include each pair only once, with the two names in alphabetical order. 
+ */
+
+SELECT H1.name, H1.grade,H2.name,H2.grade 
+FROM Highschooler H1, Highschooler H2, Likes L1, Likes L2
+WHERE L1.ID1 = L2.ID2 AND L2.ID1 = L1.ID2 AND L1.ID1 = H1.ID AND L1.ID2 = H2.ID
+      AND H1.name < H2.name
+
+/*
+Q4:
+Find all students who do not appear in the Likes table (as a student who likes or is liked) and 
+return their names and grades. Sort by grade, then by name within each grade. 
+ */
+
+SELECT H1.name, H1.grade
+FROM Highschooler H1 
+GROUP BY grade,name
+HAVING H1.id not in (SELECT id1
+                     FROM Likes)
+  AND H1.id not in (SELECT id2
+                   FROM Likes)
 
 /*
 Q5:
-Write a query to return the ratings data in a more readable format: reviewer name, movie title, stars, and ratingDate. 
-Also, sort the data, first by reviewer name, then by movie title, and lastly by number of stars. 
+For every situation where student A likes student B, but we have no information about whom B likes 
+(that is, B does not appear as an ID1 in the Likes table), return A and B's names and grades. 
  */
 
-Select name, title, stars, ratingDate
-FROM Movie join Rating on
-Rating.mID = Movie.mID
-join Reviewer on
-Reviewer.rID = Rating.rID
-Order by name,title,stars
+SELECT H1.name, H1.grade,H2.name,H2.grade 
+FROM Highschooler H1, Highschooler H2, Likes L
+WHERE L.ID1 = H1.ID AND L.ID2 = H2.ID AND H2.ID NOT IN (SELECT ID1
+                                                        FROM Likes)
+
 
 /*
 Q6:
-For all cases where the same reviewer rated the same movie twice and gave it a higher rating the second time, 
-return the reviewer's name and the title of the movie. 
+Find names and grades of students who only have friends in the same grade. Return the result sorted 
+by grade, then by name within each grade. 
  */
 
-Select name, title
-FROM Movie join Rating on
-Rating.mID = Movie.mID
-join Reviewer on
-Reviewer.rID = Rating.rID
-WHERE Reviewer.rID in (SELECT rID            --Separates based on 2 ratings only
-                       FROM Rating
-                       GROUP BY rID
-                       HAVING count(rID) = 2)
-     AND Rating.stars > (SELECT stars        --Makes sure the 2nd value is higher, might need to separate from rID
-                         FROM Rating)
-     AND Rating.mID not in (SELECT mID       --Removes entries with more than 2 movies(pretty sure this only clears the test case)
-                       FROM Rating
-                       GROUP BY mID
-                       HAVING count(mID) = 2)
+SELECT distinct name, grade
+FROM Highschooler H, Friend F
+WHERE ID NOT IN (SELECT distinct H1.ID
+             FROM Highschooler H1, Friend F, Highschooler H2
+             WHERE H1.ID = F.ID1 AND H2.ID = F.ID2 
+             AND H2.grade <> H1.grade)
+ORDER BY grade
 
 /*
 Q7:
-For each movie that has at least one rating, find the highest number of stars that movie received. Return the 
-movie title and number of stars. Sort by movie title. 
+For each student A who likes a student B where the two are not friends, find if they have a friend C in 
+common (who can introduce them!). For all such trios, return the name and grade of A, B, and C. 
  */
 
-SELECT title, max(stars)
-FROM Movie M join Rating R on
-R.mID = M.mID
-WHERE M.mID in (SELECT mID
-                FROM Rating)
-GROUP BY title
-ORDER BY title
+SELECT distinct H1.name, H1.grade, H2.name, H2.grade, H3.name, H3.grade
+FROM Highschooler H1, Likes L, Highschooler H2, Highschooler H3, Friend F1, Friend F2
+WHERE H1.ID = L.ID1 AND L.ID2 = H2.ID AND
+    H2.ID NOT IN (SELECT ID2 FROM Friend WHERE ID1 = H1.ID) AND
+    H1.ID = F1.ID1 AND F1.ID2 = H3.ID AND
+    H3.ID = F2.ID1 AND F2.ID2 = H2.ID
 
 /*
 Q8:
-For each movie, return the title and the 'rating spread', that is, the difference between highest and lowest 
-ratings given to that movie. Sort by rating spread from highest to lowest, then by movie title. 
+Find the difference between the number of students in the school and the number of different first names. 
  */
 
-SELECT title, max(stars) - min(stars) as spread
-FROM Movie M join Rating R on
-R.mID = M.mID
-WHERE M.mID in (SELECT mID
-                FROM Rating)
-GROUP BY title
-ORDER BY spread desc
+SELECT count(*) - count(distinct name)
+FROM Highschooler
 
 /*
 Q9:
-Find the difference between the average rating of movies released before 1980 and the average rating of movies 
-released after 1980. (Make sure to calculate the average rating for each movie, then the average of those averages 
-for movies before 1980 and movies after. Don't just calculate the overall average rating before and after 1980.) 
+Find the name and grade of all students who are liked by more than one other student. 
  */
 
-SELECT avg(aver1) - avg(aver2)
-FROM (SELECT title, year, avg(stars) as aver1
-	  FROM Movie join Rating on
-	  Movie.mID = Rating.mID
-	  GROUP BY title
-	  Having year < 1980) m1,
-	  (SELECT title, year, avg(stars) as aver2
-	  FROM Movie join Rating on
-	  Movie.mID = Rating.mID
-	  GROUP BY title
-	  Having year > 1980) m2
+SELECT Highschooler.name, grade
+FROM Highschooler 
+  JOIN Likes ONLikes.ID2 = Highschooler.ID
+GROUP BY ID2
+HAVING COUNT(ID2) > 1
