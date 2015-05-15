9abb14e2d8c0264188d59f92145dc1cb217373fa
Dan <dchen12812@gmail.com>
2015-05-14 17:54:57 -0700

    added answer

--- Coderbyte/longestword.js
+++ Coderbyte/longestword.js
@@ -1,11 +1,10 @@
 //Finds the largest word in a string ignoring punctuation and non empty parameter
 
-//Currently does not deal with nonspecial characters
-
 function LongestWord(sen) { 
     var word = null;
   	var longest = 0;
-  	var arrayWord = sen.split(" ");
+  	var removeSpec = sen.replace(/[^\w]/g, " "); //regex out anything not starting with a-z A-Z, \w = a-zA-Z,/g = global
+  	var arrayWord = removeSpec.split(" "); //splits string sans space into array
 
   	for(i = 0; i < arrayWord.length; i++){
       var cur = arrayWord[i];
@@ -24,3 +23,4 @@ function LongestWord(sen) {
 // keep this function call here 
 // to see how to enter arguments in JavaScript scroll down
 LongestWord(readline());                            
+
