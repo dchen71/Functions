60055b8939ec04afd90f9a4ba1f230806ba9fef9
dchen71 <dchen12812@gmail.com>
2015-07-19 15:12:29 -0700

    counts the len of each entry

--- Random Bioinfo/Fasta Analysis/fasta_analysis.py
+++ Random Bioinfo/Fasta Analysis/fasta_analysis.py
@@ -7,12 +7,15 @@ fasta = SeqIO.parse("dna.example.fasta", "fasta")
 #Converts the fasta file into dictionary entries
 sequences = {}
 for seq_record in fasta:
-	sequences[seq_record.id] = repr(seq_record.seq)
+	sequences[seq_record.id] = seq_record.seq
 
 #Reads number of fasta records in a file
 print("Number of Records : " + str(len(sequences)))
 
 #Finds the length of each seqeunce in a file
+for key,value in sequences.items():
+	print("Entry: " + key)
+	print("Length: " + str(len(value)))
 
 #Finds the open reading frames in each DNA sequence in a fasta
 
