4a30e7cae8912e47b97705c5732d226838ddff86
dchen71 <dchen12812@gmail.com>
2015-07-20 18:42:54 -0700

    added code which will filter out orf

--- Random Bioinfo/Fasta Analysis/fasta_analysis.py
+++ Random Bioinfo/Fasta Analysis/fasta_analysis.py
@@ -20,20 +20,60 @@ print("Shortest sequence: " + str(min(length)))
 
 #Finds the open reading frames in each DNA sequence in a fasta
 def orf(seq):
+	#set key as itself, values = orf
+	orf = {}
 	for key, value in sequences.items():
 		start = 0
+		stop = 3
+		kmers = []
 		if value.seq.find('ATG', start) != -1:
-			while(value.seq.find('ATG',start) != -1):
+			start = value.seq.find('ATG', start)
+			while(value.seq.find('ATG', start) != -1):
+				start = value.seq.find('ATG', start)
 				TAA = value.seq.find('TAA', start)
 				TAG = value.seq.find('TAG', start)
 				TGA = value.seq.find('TGA', start)
-				if TAA != -1 or TAG != -1 or TGA != -1:
-					break;
-			
-			#if value.seq.find('TAA') != -1 or value.seq.find('TAG') != -1 or value.seq.find('TGA') != -1:
-			#	pass
+				
+				#Logic to determine lowest index for stop codon
+				if TAA == -1:
+					if TAG == -1 and TGA != -1:
+						stop = TGA
+					elif TGA == -1 and TAG != -1:
+						stop = TAG
+					elif TGA == -1 and TAG == -1:
+						break;
+					else:
+						stop = min(TAG,TGA)
+				elif TAG == -1:
+					if TAA == -1 and TGA != -1:
+						stop = TGA
+					elif TGA == -1 and TAA != -1:
+						stop = TAA
+					elif TGA == -1 and TAA == -1:
+						break;
+					else:
+						stop = min(TAA,TGA)
+				elif TGA == -1:
+					if TAA == -1 and TAG != -1:
+						stop = TAG
+					elif TAG ==-1 and TAA != -1:
+						stop = TAA
+					elif TAA == -1 and TAG == -1:
+						break;
+					else:
+						stop = min(TAA,TAG)
+				else:
+					stop = min(TAA, TAG, TGA)
+				stop += 3
+				if (stop - start) >= 6:
+					kmer = str(value.seq[start:stop])
+					kmers.append(kmer)
 
-orf(sequences)
+				start = stop
+		orf.setdefault(key, "")
+		orf[key] = kmers
+	return orf
+print(orf(sequences))
 
 #Finds repeats of length n in the dictionary of values from fasta reads
 def repeat(n=1):
