851fed8237f493a7966c5711615791e79a51c977
Dan <dchen12812@gmail.com>
2015-08-29 01:23:12 -0700

    added fixes to ensure no empty list

--- Python/rectangle.py
+++ Python/rectangle.py
@@ -5,21 +5,19 @@ def overlap(A,B):
 
     #Checks if input is a list
     if not isinstance(A, list) or not isinstance(B, list):
-        print('not a list')
         return overlap
 
     #Validate that each point has 2 vertices
-    if len(A) < 2 | len(B) < 2:
-        print('length too short')
+    if len(A) != 2:
         return overlap
-    else:    
-        if len(A[0]) < 2 | len(A[1]) < 2 | len(B[0]) < 2 | len(B[0]) < 2:
-            print('length of array too short')
-            return overlap
+    if len(B) != 2:
+        return overlap
+    if len(A[0]) < 2 | len(A[1]) < 2 | len(B[0]) < 2 | len(B[0]) < 2:
+        print('length of array too short')
+        return overlap
 
     #Validate that each diagonal makes a rectange(diagonal points arent equal)
     if A[0][0] == A[1][0] | A[0][1] == A[1][1] | B[0][0] == B[1][0] | B[0][1] == B[1][1]:
-        print('equality check')
         return overlap
 
     #Separting the vertexes into easier to read variables and sorts them into top left to bottom right
@@ -66,5 +64,8 @@ print(overlap(rect1,rect2))
 
 #Testing
 assert overlap([[1,2],[4,-5]],[[2,2],[3,1]]) == 1 #Rectangle B completely fits inside A
+assert overlap([[1,2],[4,-5]],[[2,2],[3,-4]]) == 6 #Rectangle B stretches inside A
 assert overlap([[1,2],[4,-5]],[[2,2],[3,3]]) == 0 #Rectangle B touchs but is not in A
 assert overlap('cat','dog') == 0 #Ensures only a list as the paramters
+assert overlap([],[1,2]) == 0 #Ensures no empty lists
+assert overlap([],[]) == 0 #Ensures no empty lists
