2498621aa71772e53635f56af8ceeddb9a7ebf83
Dan <dchen12812@gmail.com>
2015-10-08 16:47:56 -0700

    added comments and converted to python3

--- Python/triangle.py
+++ Python/triangle.py
@@ -1,14 +1,15 @@
-# you can use print for debugging purposes, e.g.
-# print "this is a debug message"
+#Given a zero-indexed array A consisting of N integers, returns 1 if there exists a triangular triplet for this array and 
+#returns 0 otherwise.
 
 def solution(A):
-    # write your code in Python 2.7
     triangle = 0
     points = [0,0,0]
     if len(A) >= 3:
         A.sort()
-        for index in xrange(0, len(A) -2):
+        for index in range(0, len(A) -2):
             if A[index] + A[index + 1] > A[index + 2]:
                 triangle = 1
                 break
     return triangle
+
+assert(solution([10, 2, 5, 1, 8, 20])) == 1
