e19c1db367817cc16bb613ae10b84dd59f682945
Dan <dchen12812@gmail.com>
2015-10-08 16:52:24 -0700

    updated comments

--- Python/leapfrog.py
+++ Python/leapfrog.py
@@ -1,10 +1,10 @@
-# you can use print for debugging purposes, e.g.
-# print "this is a debug message"
+# Given three integers X, Y and D, returns the minimal number of jumps from position X to a position equal to or greater than Y.
 import math
 
 def solution(X, Y, D):
-    # write your code in Python 2.7
     jump = 0
     if X < Y:
         jump = int(math.ceil((Y-X)/float(D)))
     return jump
+
+assert(solution(10, 85, 30)) == 3
