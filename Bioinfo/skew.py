b5b95c5a7b97bad4da96d3cf78d94a2de270659b
Dan <dchen12812@gmail.com>
2015-10-08 17:25:01 -0700

    updated commeent with assert

--- Bioinfo/skew.py
+++ Bioinfo/skew.py
@@ -17,6 +17,5 @@ def skew(string):
 
 #Assert to check validity
 assert(skew("CATGGGCATCGGCCATACGCC")) == '0 -1 -1 -1 0 1 2 1 1 1 0 1 2 1 0 0 0 0 -1 0 -1 -2'
-
-text = " TAAAGACTGCCGAGAGGCCAACACGAGTGCTAGAACGAGGGGCGTAAACGCGGGTCCGAT"
-print(skew(text))
+text = "TAAAGACTGCCGAGAGGCCAACACGAGTGCTAGAACGAGGGGCGTAAACGCGGGTCCGAT"
+assert(skew(text)) == '0 0 0 0 0 1 1 0 0 1 0 -1 0 0 1 1 2 3 2 1 1 1 0 0 -1 0 0 1 1 2 1 1 1 2 2 2 1 2 2 3 4 5 6 5 6 6 6 6 6 5 6 5 6 7 8 8 7 6 7 7 7'
