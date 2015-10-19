0eae6cef6a77d8136e67c5d3ad004e236c5c3ca3
Dan <dchen12812@gmail.com>
2015-10-18 21:07:19 -0700

    init download translate

---
+++ Python/download_translate.py
@@ -0,0 +1,13 @@
+#Specify a word and language and have it download the word voiced by google translate in that language
+
+#http://translate.google.com/translate_tts?tl=it&q="abitate"
+#italian
+
+#http://translate.google.com/translate_tts?tl=cs&q="abitate"
+#czech
+
+def download_translate(word, language):
+	var lang = language
+	var query = word
+	var url = "http://translate.google.com/translate_tts?tl=" + lang + "&q=\"" + query + "\""
+	pass
