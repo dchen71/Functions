f641893f591df144df588c35dd1a964e4ba58ed1
Dan <dchen12812@gmail.com>
2015-10-21 01:16:48 -0700

    added version of fewest edit distance search

---
+++ Bioinfo/fewest_edit.py
@@ -0,0 +1,28 @@
+#Your function should take arguments p (pattern), t (text) and should return the edit distance of the match between P and T with the fewest edits. 
+
+def fewEditDistance(x, y):
+    # Create distance matrix
+    D = []
+    for i in range(len(x)+1):
+        D.append([0]*(len(y)+1))
+    # Initialize first row and column of matrix
+    for i in range(len(x)+1):
+        D[i][0] = i
+    for i in range(len(y)+1):
+        D[0][i] = 0
+    # Fill in the rest of the matrix
+    for i in range(1, len(x)+1):
+        for j in range(1, len(y)+1):
+            distHor = D[i][j-1] + 1
+            distVer = D[i-1][j] + 1
+            if x[i-1] == y[j-1]:
+                distDiag = D[i-1][j-1]
+            else:
+                distDiag = D[i-1][j-1] + 1
+            D[i][j] = min(distHor, distVer, distDiag)
+    
+    # Lowest Edit distance is the min in the bottom row
+    return min(D[-1])
+
+#Tests
+assert(fewEditDistance('GCGTATGC','TATTGGCTATACGGTT')) == 2
