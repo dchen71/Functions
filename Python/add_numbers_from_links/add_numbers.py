312746728eadddedcb87eac9319a1d6453057f95
Dan <dchen12812@gmail.com>
2015-11-07 21:48:00 -0800

    implemented version which will add sum of values in a table

--- Python/add_numbers_from_links/add_numbers.py
+++ Python/add_numbers_from_links/add_numbers.py
@@ -3,17 +3,25 @@
 import urllib.request
 from bs4 import BeautifulSoup
 
+#Takes a url and returns the total of numbers between the spans
+def add_numbers(url):
+	total = 0
+	
+	#Reads the data from the url
+	with urllib.request.urlopen(url) as repsonse:
+		html = repsonse.read()
+
+	#Beautitful soup parser
+	soup = BeautifulSoup(html, "html.parser")
+
+	# Retrieve all of the anchor tags
+	spans = soup.find_all('span')
+	for span in spans:
+	   total += int(span.contents[0])
+	
+	return total
+
+
+#Tests
 url = "http://pr4e.dr-chuck.com/tsugi/mod/python-data/data/comments_42.html"
-with urllib.request.urlopen(url) as repsonse:
-	html = repsonse.read()
-
-soup = BeautifulSoup(html, "html.parser")
-
-# Retrieve all of the anchor tags
-tags = soup.find_all('a')
-for tag in tags:
-   # Look at the parts of a tag
-   print('TAG:',tag)
-   print('URL:',tag.get('href', None))
-   print('Contents:',tag.contents[0])
-   print('Attrs:',tag.attrs)
+assert(add_numbers(url)) == 2553
