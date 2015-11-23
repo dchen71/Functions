5f9c66afa9781d87c6546ef58962d6e396911fa4
dchen71 <dchen12812@gmail.com>
2015-11-23 13:38:24 -0800

    added general logic

--- Bioinfo/motifenumeration.py
+++ Bioinfo/motifenumeration.py
@@ -5,8 +5,23 @@ Output: All (k, d)-motifs in Dna.
 
 def motifenumeration(Dna, k, d):
 	patterns = set()
-	for i in range(len(Dna) - k):
-		pattern = substr(Dna, i , k)
-		#for loop to check rest of the string
-			#if pattern is in string with at most d mismatch add pattern to patterns
+	for seq in Dna:
+		for i in range(len(seq) - k):
+			pattern = seq[i:i+k]
+			for j in range(len(seq) - k):
+				test = seq[j:j+k]
+				mismatches = 0
+				for k in range(k):
+					if mismatches < d:
+						if pattern[k] != test[k]:
+							mismatches += 1
+					else:
+						break
 	return patterns
+
+k = 3
+d = 1
+input = ['ATTTGGC', 'TGCCTTA', 'CGGTATC', 'GAAAATT']
+
+patterns = motifenumeration(input, k, d)
+print(patterns)
