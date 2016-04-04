f75871579a35956ae4667f2dcadbe612846e6986
Dan <dchen12812@gmail.com>
2016-04-04 00:26:52 -0700

    updated leader router

---
+++ Node/confusion/leaderRouter.js
@@ -0,0 +1,52 @@
+// Node module to support leaders endpoint
+
+
+var leaderRouter = express.Router();
+
+leaderRouter.use(bodyParser.json());
+
+leaderRouter.route('/')
+.all(function(req,res,next) {
+      res.writeHead(200, { 'Content-Type': 'text/plain' });
+      next();
+})
+
+.get(function(req,res,next){
+        res.end('Will send all the leaders to you!');
+})
+
+.post(function(req, res, next){
+    res.end('Will add the leader: ' + req.body.name + ' with details: ' + req.body.description);    
+})
+
+.delete(function(req, res, next){
+        res.end('Deleting all leaders');
+});
+
+leaderRouter.route('/:leaderId')
+.all(function(req,res,next) {
+      res.writeHead(200, { 'Content-Type': 'text/plain' });
+      next();
+})
+
+.get(function(req,res,next){
+        res.end('Will send details of the leader: ' + req.params.leaderId +' to you!');
+})
+
+.put(function(req, res, next){
+        res.write('Updating the leader: ' + req.params.leaderId + '\n');
+    res.end('Will update the leader: ' + req.body.name + 
+            ' with details: ' + req.body.description);
+})
+
+.delete(function(req, res, next){
+        res.end('Deleting leader: ' + req.params.leaderId);
+});
+
+app.use('/leaders',leaderRouter);
+
+app.use(express.static(__dirname + '/public'));
+
+app.listen(port, hostname, function(){
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
