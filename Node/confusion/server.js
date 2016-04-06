36527e98126bffa3b79bf6a2165e4d7490762fe7
Dan <dchen12812@gmail.com>
2016-04-05 22:59:21 -0700

    added requirements for server

---
+++ Node/confusion/server.js
@@ -0,0 +1,15 @@
+// Test server utilizing express for dishes/promotions/leaders
+
+var express = require('express');
+var morgan = require('morgan');
+var bodyParser = require('body-parser');
+var dishRouter = require('dishRouter');
+var leaderRouter = require('leaderRouter');
+var promoRouter = require('promoRouter');
+
+var hostname = 'localhost';
+var port = 3000;
+
+var app = express();
+
+app.use(morgan('dev'));
